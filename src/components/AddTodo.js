d1219e55e38d648a606d6c926506e2548fece0a1
Christopher Pedersen <chris@topherpedersen.com>
2019-12-30 23:00:52 -0600

    added additional handleAddTodo method to AddTodo component and render/return method

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,11 +1,32 @@
 // components/AddTodo.js
 
-// ... other imports
+import React from 'react';
 import { connect } from 'react-redux';
 import { addTodo } from '../redux/actions';
 
 class AddTodo extends React.Component {
-  // ... component implementation
+  // ...
+  handleAddTodo = () => {
+    // dispatches actions to add todo
+    this.props.addTodo(this.state.input);
+
+    // sets state back to empty string
+    this.setState({ input: '' });
+  };
+
+  render() {
+    return(
+      <div>
+        <input  
+          onChange={ e => this.updateInput(e.target.value) }
+          value={this.state.input}
+        />
+        <button className="add-todo" onClick={this.handleAddTodo}>
+          Add Todo
+        </button>
+      </div>
+    );
+  }
 }
 
 export default connect(
