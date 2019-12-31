e71252695c15dd113620db3d9da1563540c697ae
Christopher Pedersen <chris@topherpedersen.com>
2019-12-30 22:27:47 -0600

    following along with official redux tutorial, redux not yet imported (app currently broken at this point)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
-// import './App.css';
+import ReactDOM from 'react-dom';
+
+/*
+import { Provider } from 'react-redux';
+import store from './store';
+*/
 
 class App extends React.Component {
   constructor(props) {
---
+++ src/store.js
@@ -0,0 +1,1 @@
+console.log("todo: create redux store");
